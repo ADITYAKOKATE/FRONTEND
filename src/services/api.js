947d44a4f70a79c13e521237755d599464b2c133
Aditya Rajendra Kokate <144372455+ADITYAKOKATE@users.noreply.github.com>
2025-10-06 00:34:59 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://nasaspaceapps-2.onrender.com';
+const API_BASE_URL = 'https://nasaspaceapps-av71.onrender.com';
 
 // Create axios instance
 const api = axios.create({
